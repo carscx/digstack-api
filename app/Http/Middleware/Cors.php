ad1cff32effb608aef8df67b4e4901e271a0cbcc
Carlos Santos <csantos@digbang.com>
2022-04-04 14:50:41 -0300

    Fix

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class Cors
 {
@@ -10,13 +11,14 @@ class Cors
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+      ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,19 +18,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['cors'])->group(function () {
+Route::post('auth/login', [AuthController::class, 'login'])->name('login');
+Route::post('auth/register', [AuthController::class, 'register']);
 
-    Route::post('auth/login', [AuthController::class, 'login'])->name('login');
-    Route::post('auth/register', [AuthController::class, 'register']);
-
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::prefix('auth')->group(function () {
-            // Route::post('register', [AuthController::class, 'register']);
-            Route::post('logout', [AuthController::class, 'logout']);
-        });
-
-        Route::resource('users', UserController::class);
-        Route::resource('posts', PostController::class);
-        Route::resource('comments', CommentController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('auth')->group(function () {
+        // Route::post('register', [AuthController::class, 'register']);
+        Route::post('logout', [AuthController::class, 'logout']);
     });
+
+    Route::resource('users', UserController::class);
+    Route::resource('posts', PostController::class);
+    Route::resource('comments', CommentController::class);
 });
