847e1c7d68516348e5544997082a60aea37ec533
Carlos Santos <csantos@digbang.com>
2022-04-04 17:55:40 -0300

    Test api

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/hola', function () {
     return HandlerResponse::responseJSON([
         'message' => 'Hola usuario'
-    ], 401);
+    ], 200);
 });
 Route::post('auth/login', [AuthController::class, 'login'])->name('login');
 Route::post('auth/register', [AuthController::class, 'register']);
