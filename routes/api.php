bdb00800289bf479514bd16089aadd2403b57c70
Carlos Santos <csantos@digbang.com>
2022-04-04 17:55:15 -0300

    Test api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Actions\Handlers\HandlerResponse;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
@@ -19,7 +20,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/hola', function () {
-    return 'hola';
+    return HandlerResponse::responseJSON([
+        'message' => 'Hola usuario'
+    ], 401);
 });
 Route::post('auth/login', [AuthController::class, 'login'])->name('login');
 Route::post('auth/register', [AuthController::class, 'register']);
